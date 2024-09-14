00bb693fdd52ef7f75a9fc413cdc6fcec2c21e29
joanabezerra <joanafiap@gmail.com>
2024-09-14 11:06:23 -0300

    tagueamento

--- js/metodos-de-ativacao.js
+++ js/metodos-de-ativacao.js
@@ -1,85 +1,86 @@
-console.log('Oi');
+// console.log('Oi');
 
-function pegarValorDoElement(elemento) {
+// function pegarValorDoElement(elemento) {
 
-    const atributoElement = elemento.getAttribute('data-gtm-element');    
-    return atributoElement;
+//     const atributoElement = elemento.getAttribute('data-gtm-element');    
+//     return atributoElement;
 
-}
+// }
 
-function pegarValorDoCategory(elemento) {
+// function pegarValorDoCategory(elemento) {
 
-    const atributoCategory = elemento.getAttribute('data-gtm-category');
-    return atributoCategory;
+//     const atributoCategory = elemento.getAttribute('data-gtm-category');
+//     return atributoCategory;
 
-}
+// }
 
-function pegarValorDoAction(elemento) {
+// function pegarValorDoAction(elemento) {
 
-    const atributoAction = elemento.getAttribute('data-gtm-action');
-    return atributoAction;
+//     const atributoAction = elemento.getAttribute('data-gtm-action');
+//     return atributoAction;
     
-}
+// }
 
-function pegarValorDoLabel(elemento) {
+// function pegarValorDoLabel() {
 
-    const atributoLabel = elemento.getAttribute('data-gtm-label');
-    return atributoLabel;
+//     var div1 = document.getElementById("aceite-dados");
+//     const atributoLabel = div1.getAttribute('data-gtm-label');
+//     alert(atributoLabel);
     
-}
+// }
 
-function aceito(elemento) {
+// function aceito(elemento) {
 
-    console.log('clicou aceito!');
+//     console.log('clicou aceito!');
     
-    // esconder div de termos
-    document.getElementById(elemento).style.display = 'none';
+//     // esconder div de termos
+//     document.getElementById(elemento).style.display = 'none';
 
-    // Elemento do botão
-    var idBotao = document.getElementById('aceito-termos');
+//     // Elemento do botão
+//     var idBotao = document.getElementById('aceito-termos');
 
-    const element = pegarValorDoElement(idBotao);
-    const category = pegarValorDoCategory(idBotao);
-    const action = pegarValorDoAction(idBotao);
-    const label = pegarValorDoLabel(idBotao);
+//     const element = pegarValorDoElement(idBotao);
+//     const category = pegarValorDoCategory(idBotao);
+//     const action = pegarValorDoAction(idBotao);
+//     const label = pegarValorDoLabel(idBotao);
 
-    window.dataLayer = window.dataLayer || [];
+//     window.dataLayer = window.dataLayer || [];
     
-    window.dataLayer.push({
-        event: 'eventGA',
-        element: element,
-        category: category,
-        action: action,
-        label: label
-    });
+//     window.dataLayer.push({
+//         event: 'eventGA',
+//         element: element,
+//         category: category,
+//         action: action,
+//         label: label
+//     });
     
-}
+// }
 
-// pegarValorDoCategory('aceito-termos');
+// // pegarValorDoCategory('aceito-termos');
 
-function naoaceito(elemento) {
+// function naoaceito(elemento) {
     
-    console.log('clicou não aceito!');
+//     console.log('clicou não aceito!');
 
-    // esconder div de termos
-    document.getElementById(elemento).style.display = 'none';
+//     // esconder div de termos
+//     document.getElementById(elemento).style.display = 'none';
 
-    // Elemento do botão
-    var idBotao = document.getElementById('nao-aceito-termos');
+//     // Elemento do botão
+//     var idBotao = document.getElementById('nao-aceito-termos');
 
-    const element = pegarValorDoElement(idBotao);
-    const category = pegarValorDoCategory(idBotao);
-    const action = pegarValorDoAction(idBotao);
-    const label = pegarValorDoLabel(idBotao);
+//     const element = pegarValorDoElement(idBotao);
+//     const category = pegarValorDoCategory(idBotao);
+//     const action = pegarValorDoAction(idBotao);
+//     const label = pegarValorDoLabel(idBotao);
 
-    window.dataLayer = window.dataLayer || [];
+//     window.dataLayer = window.dataLayer || [];
     
-    window.dataLayer.push({
-        event: 'eventGA',
-        element: element,
-        category: category,
-        action: action,
-        label: label
-    });
+//     window.dataLayer.push({
+//         event: 'eventGA',
+//         element: element,
+//         category: category,
+//         action: action,
+//         label: label
+//     });
 
-}
+// }
